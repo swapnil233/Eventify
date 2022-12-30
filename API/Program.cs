d849e3ac97c8d5f4f913c6570e0cebf74a1beb9c
Hasan Iqbal <hasan.iqbal@ec.gc.ca>
2022-12-30 13:06:13 -0500

    Project init with Domain Driven Architecture

---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Registers endpoints
+app.MapControllers();
+
+app.Run();
