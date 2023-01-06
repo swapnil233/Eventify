7d84bea1d2c268468b2bac7721f838a0ddbb9906
Hasan Iqbal <hasan.iqbal@ec.gc.ca>
2023-01-06 16:27:33 -0500

    Use extension method to bring in services

--- API/Program.cs
+++ API/Program.cs
@@ -1,36 +1,19 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Enable controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// SQLite DB Service
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-// Cors Service
-builder.Services.AddCors(options => {
-    options.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
+// Add all services via extension method
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Middleware
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Redirect to HTTPS
+app.UseHttpsRedirection();
 
 // Use CORS
 app.UseCors("CorsPolicy");
