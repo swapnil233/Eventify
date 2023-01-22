2b5adcd309837ba2da59bd40818324c71568aaf0
Hasan Iqbal <swapniliqbal@gmail.com>
2023-01-22 16:53:59 -0500

    Selecting Activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,39 +5,39 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    // GET /api/activities
+    // Get activities
     [HttpGet("/api/activities/")]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
         return await Mediator.Send(new List.Query());
     }
 
-    // GET /api/activities/{id}
+    // Get activity
     [HttpGet("/api/activities/{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return await Mediator.Send(new Details.Query{Id = id});
+        return await Mediator.Send(new Details.Query { Id = id });
     }
 
-    // POST /api/activities
+    // Create activity
     [HttpPost("/api/activities/")]
     public async Task<IActionResult> CreateActivity(Activity activity)
     {
-        return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
     }
 
-    // PUT /api/activities/{id}
+    // Edit activity
     [HttpPut("/api/activities/{id}")]
     public async Task<IActionResult> EditActivity(Guid id, Activity activity)
     {
         activity.Id = id;
-        return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
     }
 
-    // DELETE /api/activities/{id}
+    // Delete activity
     [HttpDelete("/api/activities/{id}")]
     public async Task<IActionResult> DeleteActivity(Guid id)
     {
-        return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        return Ok(await Mediator.Send(new Delete.Command { Id = id }));
     }
 }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import '../layout/styles.css'
 import axios from 'axios';
-import { Container, Header } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
 import NavBar from './NavBar';
+import { Activity } from '../models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get('https://localhost:5000/api/activities')
@@ -15,11 +17,24 @@ function App() {
       })
   }, []);
 
+  const handleSelectActivity = (id : string) => {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  }
+
   return (
     <>
       <NavBar />
       <Container style={{marginTop:'7em'}}>
-        <ActivityDashboard activities={activities}/>
+        <ActivityDashboard 
+          activities={activities}
+          selectedActivity = {selectedActivity}
+          selectActivity = {handleSelectActivity}
+          cancelSelectActivity = {handleCancelSelectActivity}
+          />
       </Container>
     </>
   );
